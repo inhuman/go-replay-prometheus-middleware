7026b1f88c99acd2d18c172a81e7165ff2f1297c
id <msgexec@gmail.com>
2020-04-10 12:28:15 +0300

    feat(app): read request

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"github.com/Depado/ginprom"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -34,23 +37,76 @@ func main() {
 		buf := make([]byte, len(encoded)/2)
 		hex.Decode(buf, encoded)
 
-		// First byte indicate payload type, possible values:
-		//  1 - Request
-		//  2 - Response
-		//  3 - ReplayedResponse
-		payloadType := buf[0]
+		r := bytes.NewReader(buf)
 
-		switch payloadType {
-		case '1': // Request
-			m.With(prometheus.Labels{"payload": "request"}).Add(1)
-
-		case '2': // Original response
-			m.With(prometheus.Labels{"payload": "original_response"}).Add(1)
-
-		case '3': // Replayed response
-			m.With(prometheus.Labels{"payload": "replayed_response"}).Add(1)
+		_, err := ReadHTTPFromFile(r)
+		if err != nil {
+			fmt.Printf("error read from stdin: %s\n", err)
 		}
+
+		//// First byte indicate payload type, possible values:
+		////  1 - Request
+		////  2 - Response
+		////  3 - ReplayedResponse
+		//payloadType := buf[0]
+		//
+		//switch payloadType {
+		//case '1': // Request
+		//	m.With(prometheus.Labels{"payload": "request"}).Add(1)
+		//	os.Stdout.Write(encode(buf))
+		//
+		//case '2': // Original response
+		//	m.With(prometheus.Labels{"payload": "original_response"}).Add(1)
+		//
+		//case '3': // Replayed response
+		//	m.With(prometheus.Labels{"payload": "replayed_response"}).Add(1)
+		//}
 	}
+}
+
+type Connection struct {
+	Request  *http.Request
+	Response *http.Response
+}
+
+func ReadHTTPFromFile(r io.Reader) ([]Connection, error) {
+	buf := bufio.NewReader(r)
+	stream := make([]Connection, 0)
+
+	for {
+		req, err := http.ReadRequest(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return stream, err
+		}
+
+		fmt.Printf("req: %+v\n", req)
+
+		//resp, err := http.ReadResponse(buf, req)
+		//if err != nil {
+		//	return stream, err
+		//}
+		//
+		////save response body
+		//b := new(bytes.Buffer)
+		//io.Copy(b, resp.Body)
+		//resp.Body.Close()
+		//resp.Body = ioutil.NopCloser(b)
+		//
+		//stream = append(stream, Connection{Request: req, Response: resp})
+	}
+	return stream, nil
+
+}
+
+func encode(buf []byte) []byte {
+	dst := make([]byte, len(buf)*2+1)
+	hex.Encode(dst, buf)
+	dst[len(dst)-1] = '\n'
+
+	return dst
 }
 
 func listener() error {
